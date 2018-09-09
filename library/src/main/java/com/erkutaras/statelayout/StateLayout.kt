7060132470ed6df6de89e92772de4c983faa9a63
erkutaras <erkutaras@gmail.com>
2018-09-09 22:04:22 +0300

    create library project add StateLayout create sample project

---
+++ library/src/main/java/com/erkutaras/statelayout/StateLayout.kt
@@ -0,0 +1,18 @@
+package com.erkutaras.statelayout
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+/**
+ * Created by erkutaras on 9.09.2018.
+ */
+class StateLayout @JvmOverloads constructor(context: Context,
+                                            attrs: AttributeSet? = null,
+                                            defStyleAttr: Int = 0)
+    : FrameLayout(context, attrs, defStyleAttr) {
+
+    init {
+        TODO("inflate all state views")
+    }
+}
---
+++ sample/src/main/java/com/erkutaras/statelayout/sample/StateLayoutSampleActivity.kt
@@ -0,0 +1,12 @@
+package com.erkutaras.statelayout.sample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StateLayoutSampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_state_layout_sample)
+    }
+}
